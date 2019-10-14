038f5b9da162fca25954fc844c152fe2decaa48f
Jem <jemjudilla@hotmail.com>
2019-10-14 22:03:32 +0800

    commit week2

--- scripts/week2.js
+++ scripts/week2.js
@@ -5,20 +5,31 @@ function doPayment() {
     var amountExpected = parseFloat(document.getElementById('expectedamount').value);
     
     computeSavings(saved, rate, years, amountExpected)
-    
+  
 }
 
 function computeSavings(initialSavings, annualRate, nyears, expectedamt){
 var p = initialSavings;
-var r = annualRate/12;
+var rate = 1 + (annualRate/12);
 var years = nyears;
 var t;
+var message = "";
+var total = 0;
+var infomsg = "";
 //loop here
-for (t = 1; t < years; t++) {
-var total = p*(1+(r/12))^(12*t);
-var message = "Year " + t + ": " + total +"\n";
-p = total;
+for (t = 1; t <= years; t++) {
+   total = (p*(Math.pow(rate, (12*t)))).toFixed(2);
+   message += "<b>Year " + t + ": </b>" + total + "<br><br>";
+   
+}
+    document.getElementById('output').innerHTML = message;
+ var difference1 = Math.abs(expectedamt - total).toFixed(2);
+ var difference2 = Math.abs(total - expectedamt).toFixed(2);
+     if (total < expectedamt) {
+        infomsg = "Sorry! The amount you will get in the end is " + difference1 + " less than your expected amount."
+    } else {
+        infomsg = "Great! The amount you will get in the end is " + difference2 + " more than your expected amount."
+    }
+   document.getElementById("message").innerHTML = infomsg;
+}
 
-}
-    document.getElementById('output').value = message;
-}
