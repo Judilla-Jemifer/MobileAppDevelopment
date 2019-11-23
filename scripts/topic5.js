012f41c117fdbb370db8fb06e6b02fcc183a80d1
Jem <jemjudilla@hotmail.com>
2019-11-23 16:29:23 +0800

    topic5

--- scripts/topic5.js
+++ scripts/topic5.js
@@ -3,7 +3,7 @@ if (typeof(Storage) !== "undefined") {
   // Store
   localStorage.setItem("name", "Jemifer Judilla");
   // Retrieve
-  document.getElementById("getoutput").innerHTML = "My name is " + localStorage.getItem("name") +".";
+  document.getElementById("getoutput").innerHTML = "My name is " + localStorage.getItem("name") + ".";
 } else {
   document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
 }
@@ -16,7 +16,7 @@ if (typeof(Storage) !== "undefined") {
   // Retrieve
   document.getElementById("getoutput").innerHTML = "I was born in the year " + localStorage.getItem("year") + ".";
 } else {
-  document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+  document.getElementById("getoutput").innerHTML = "Sorry, your browser does not support Web Storage...";
 }
 }
 
@@ -27,7 +27,7 @@ if (typeof(Storage) !== "undefined") {
   // Retrieve
   document.getElementById("getoutput").innerHTML = "My birth month is in " + localStorage.getItem("month") + ".";
 } else {
-  document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+  document.getElementById("getoutput").innerHTML = "Sorry, your browser does not support Web Storage...";
 }
 }
 
@@ -38,6 +38,41 @@ if (typeof(Storage) !== "undefined") {
   // Retrieve
   document.getElementById("getoutput").innerHTML = "I have " + localStorage.getItem("kids") + " kids. Both are boys aged 9 and 5 as of 2019.";
 } else {
-  document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+  document.getElementById("getoutput").innerHTML = "Sorry, your browser does not support Web Storage...";
 }
 }
+
+
+var message = ["Only a life lived for others is a life worthwhile. - Albert Einstein","Because I have loved life, I shall have no sorrow to die. - Amelia Burr", "Everything has beauty, but not everyone sees it. - Confucius","Nothing will work unless you do. - Maya Angelou"];
+
+localStorage.setItem("quotes",JSON.stringify(message));
+
+        var txt = "";
+        var i;
+        
+        //Loop to get a list from the data
+        for (i=0; i<message.length; i++) {
+            txt += "<p>" + message[i] + " </p>";        
+    }
+    
+         document.getElementById("arrayoutput").innerHTML = txt;
+        
+
+function addarray() {
+    var input = document.getElementById('inputquote').value;
+    
+    
+    var retrievedData = localStorage.getItem("quotes");
+    var message2 = JSON.parse(retrievedData);
+    
+    message2.push(input);
+    localStorage.setItem("message2", JSON.stringify(message2));
+      //Loop to get a list from the data
+        for (i=message2.length-1; i<message2.length; i++) {
+            txt += "<p>" + message2[i] + " </p>";        
+    }
+    
+         document.getElementById("arrayoutput").innerHTML = txt;
+}
+   
+
